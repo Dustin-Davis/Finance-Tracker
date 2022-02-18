7ab0f6512322e58a5c4273f43b5b6f572f7d818a
Dustin-Davis <drpookydavis@comcast.net>
2022-02-17 17:51:08 -0800

    import needed stuff

---
+++ client/components/Main/List/list.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { List as MUIList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
+// import { Delete, MoneyOff } from '@material-ui/icons';
+
+// import useStyles from './styles';
+
+const List = () => {
+  // const classes = useStyles();
+  return (
+  // <MUIList>
+<div></div>
+  // </MUIList>
+  );
+};
+
+export default List;
