906c6a142ec1ba61ecb54c47b4f42b81a3241149
Dustin-Davis <drpookydavis@comcast.net>
2022-02-17 18:24:42 -0800

    create list structurn

--- client/components/Main/List/list.jsx
+++ client/components/Main/List/list.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
-// import { List as MUIList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
-// import { Delete, MoneyOff } from '@material-ui/icons';
+import { List as MUIList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
+import { Delete, MoneyOff } from '@material-ui/icons';
 
-// import useStyles from './styles';
+import useStyles from './styles';
 
 const List = () => {
-  // const classes = useStyles();
+  const classes = useStyles();
+
+  const transactions = [
+    { id: 1, type: 'Income', category: 'Salary', amount: 75, date: 'Thurs Feb 17' },
+    { id: 2, type: 'Expense', category: 'Car', amount: 750, date: 'Thurs Feb 18' },
+    { id: 3, type: 'Income', category: 'Salary', amount: 7500, date: 'Thurs Feb 19' }
+
+  ];
+
   return (
-  // <MUIList>
-<div></div>
-  // </MUIList>
+    <MUIList dense={false} className={classes.list}>
+      {transactions.map(transactions => (
+        <Slide direction='down' in mountOnEnter unmountOnExit key={transactions.id}>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar className={transactions.type === 'Income' ? classes.avatarIncome : classes.avatarExpense }>
+                <MoneyOff />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={transactions.category} secondary={`$${transactions.amount} - ${transactions.date}`}/>
+            <ListItemSecondaryAction>
+              <IconButton edge='end' aria-label='delete'>
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Slide>
+      ))}
+    </MUIList>
   );
 };
 
--- client/components/Main/main.jsx
+++ client/components/Main/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 import useStyles from './styles';
 import Form from './Form/form';
-
+import List from './List/list';
 const Main = () => {
   const classes = useStyles();
   return (
@@ -16,7 +16,7 @@ const Main = () => {
       <CardContent className={classes.CardContent}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            {/* <List /> */}
+            <List />
           </Grid>
         </Grid>
       </CardContent>
