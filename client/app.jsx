6276728d2f72097881741236deed589e75cf5b23
Dustin-Davis <drpookydavis@comcast.net>
2022-03-02 20:58:09 -0800

    added sign-in and sign-up pages

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
 import Home from './pages/home';
+import parseRoute from './lib/parse-route';
+import NotFound from './pages/not-found';
+import SignIn from './pages/sign-in';
+import SignUp from './pages/sign-up';
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', event => {
+      this.setState({ route: parseRoute(window.location.hash) });
+    });
+  }
+
+  renderPage() {
+    const { route } = this.state;
+    if (route.path === '') {
+      return <SignIn />;
+    }
+    if (route.path === 'home') {
+      return <Home />;
+    }
+    if (route.path === 'sign-up') {
+      return <SignUp />;
+    }
+    return <NotFound />;
+  }
 
   render() {
-    return <Home />;
+    return (
+      <>
+        {this.renderPage()}
+      </>
+    );
   }
 }
